8e95879d1a6946fe69d30bc73172c2a25a555f19
ytchitiya-a11y <ytchitiya@gmail.com>
2026-01-19 15:47:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ import cors from "cors";
 import aiRoute from "./routes/ai.js";
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/ai", aiRoute);
 
-app.listen(3000, () => {
-  console.log("Backend running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on", PORT);
 });
